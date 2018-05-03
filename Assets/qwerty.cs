6dc63c60c4c5c6afe1dc26769a0a304ed16115b6
vilkapolaka <38940816+vilkapolaka@users.noreply.github.com>
2018-05-03 14:13:50 +0300

    nothing

---
+++ Assets/qwerty.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class qwerty : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(h, 0, v);
+        rb.AddForce(dir);
+    }
+}
